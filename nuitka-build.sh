d7772b545f66236bbcc70874dbbbbfe5ecc7e7e1
Wilson.Huang <114924470+WilsonHuangDev@users.noreply.github.com>
2025-04-02 00:57:27 +0800

    修复Nuitka构建脚本中设置软件Logo选项错误问题

--- nuitka-build.sh
+++ nuitka-build.sh
@@ -12,7 +12,7 @@ rm -rf ./dist 2> /dev/null
 python -m nuitka \
 --standalone \
 --windows-console-mode=disable \
---windows-icon=./Assets/icon.ico \
+--windows-icon-from-ico=./Assets/icon.ico \
 --include-package=PasswdChanger \
 --include-package=modules \
 --output-dir=./dist/Windows-Login-Helper \
